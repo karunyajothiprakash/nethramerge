7bfd9c1f88719983867fd077711b00c8907bc1a6
ERP <shastikaglobalimpexpvtltd@gmail.com>
2026-06-06 12:36:20 +0530

    face tracking attendence

--- src/App.tsx
+++ src/App.tsx
@@ -128,6 +128,7 @@ import FinancialReports from "./pages/payments/FinancialReports";
 import EmployeeDirectory from "./pages/employees/EmployeeDirectory";
 import Attendance from "./pages/employees/Attendance";
 import RolesPermissions from "./pages/employees/RolesPermissions";
+import FaceAttendanceGuard from '@/guards/FaceAttendanceGuard';
 import FaceAttendance from "./pages/FaceAttendance.tsx";
 import RegisterFace from "./pages/RegisterFace.tsx";
 
@@ -319,7 +320,7 @@ const App = () => (
               {/* Employees */}
               <Route path="/employees" element={<EmployeeDirectory />} />
               <Route path="/employees/attendance" element={<Attendance />} />
-              <Route path="/employees/face-attendance" element={<FaceAttendance />} />
+              <Route path="/employees/face-attendance" element={<FaceAttendanceGuard><FaceAttendance /></FaceAttendanceGuard>} />
               <Route path="/employees/register-face" element={<RegisterFace />} />
               <Route path="/employees/roles" element={<RolesPermissions />} />
 
